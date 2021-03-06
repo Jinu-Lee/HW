234a04b9945cb8dcd0286b868ca8f52bbdabed81
Jinu-Lee <jinulee@kakao.com>
2021-03-06 17:52:45 +0900

    2nd week of 2021-1

---
+++ 2021/Spring/Mobile_App_Programming_Lab/week2/app/src/main/java/edu/skku/MAP/WA2/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.skku.MAP.WA2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1, btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hi, I am Jin Woo Lee.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Good bye, see you.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
